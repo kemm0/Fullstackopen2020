ab1d230514812e55d70d56a1b458976a8d114c21
kemm0 <juho.ktokoi@outlook.com>
2020-12-24 02:14:29 +0200

    exercises 4.15-4.17 done

--- osa4/blogilistasovellus/controllers/blogs.js
+++ osa4/blogilistasovellus/controllers/blogs.js
@@ -1,23 +1,28 @@
 const blogsRouter = require('express').Router()
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const logger = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   logger.info(blogs)
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
   const savedBlog = await blog.save()
+  user.notes = user.notes.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog.toJSON)
 })
 

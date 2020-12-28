26653cb0f3b8dae9e63f020e0cfb4c687847e230
kemm0 <juho.ktokoi@outlook.com>
2020-12-28 23:29:37 +0200

    Fixed missing dotenv-configuration in index.js

--- osa4/blogilistasovellus/index.js
+++ osa4/blogilistasovellus/index.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
 const logger = require('./utils/logger')
+require("dotenv").config()
 
 
 const server = http.createServer(app)
